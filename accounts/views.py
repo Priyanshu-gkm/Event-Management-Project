d335c0a6eceb194de5dca238640d463398ac3821
Priyanshu Arora <priyanshu@gkmit.co>
2023-10-30 13:55:50 +0530

    feat: add test cases and url config for accounts app

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from accounts.models import Account
+from accounts.serializers import AccountSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.generics import ListCreateAPIView , RetrieveUpdateDestroyAPIView
+from django.core.exceptions import ObjectDoesNotExist
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import authenticate
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+
+class AccountLCAPIView(ListCreateAPIView):
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['username']
+    
+    
+class AccountRUDAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['username']    
+    
+
+class LoginView(APIView):
+    pass
+    
+class LogoutView(APIView):
+    pass
