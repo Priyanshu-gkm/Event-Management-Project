5a9fe271d87689ab4264bf60c5aaa2ce6dbd26e5
Priyanshu Arora <priyanshu@gkmit.co>
2023-11-03 18:52:31 +0530

    added failing test cases for tickets CR

--- events_tickets/ticket_urls.py
+++ events_tickets/ticket_urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from events_tickets import views
 
 urlpatterns = [
+    path("",views.TicketLC.as_view(),name="LC-ticket"),
+    path("<int:pk>/",views.TicketRUD.as_view(),name="RUD-ticket"),
 ]
