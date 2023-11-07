5b234b5c12ac23a1716dbef26e043ac60f69ddfd
Priyanshu Arora <priyanshu@gkmit.co>
2023-11-07 18:48:04 +0530

    moved secret key to env

--- Event_Management/settings.py
+++ Event_Management/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-r#_!2hyy+3$l#+)du4_a3wycg7=fwg@96g9)(s5&+*9ehps9u2"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
